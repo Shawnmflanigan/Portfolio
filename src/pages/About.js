7cac15c47f9032ec1e263baa00c10adeb1cc1647
Shawn Flanigan <shawnmflanigan@gmail.com>
2021-04-08 11:17:29 -0700

    add page functionality

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import Hero from "../components/Hero";
+import Card from "../components/Card";
+// import Navbar from "../components/Navbar"
+import Jumbotron from "../components/Jumbotron"
+
+function About() {
+    return (
+            <div>
+                {/* <Hero backgroundImage="https://i.imgur.com/qkdpN.jpg"> */}
+                    <Jumbotron />
+                    <Card />
+                {/* </Hero> */}
+        </div>
+    ) 
+    
+};
+
+export default About;
---
+++ src/pages/Music.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import Hero from "../components/Hero";
+import Card from "../components/Card";
+import Jumbotron from "../components/Jumbotron"
+
+function Music() {
+    return (
+            <div>
+                {/* <Hero backgroundImage="https://i.imgur.com/qkdpN.jpg"> */}
+                    <Jumbotron />
+                    <Card />
+                {/* </Hero> */}
+        </div>
+    ) 
+    
+};
+
+export default Music;
